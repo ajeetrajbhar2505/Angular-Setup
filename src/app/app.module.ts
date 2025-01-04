290573a3bc09448a5387ec743be19058d79a5222
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-01-04 18:07:51 +0530

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component
   ],
   imports: [
     BrowserModule,
